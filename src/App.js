8270b1cbd7df848ce51dcf7c70dda0716300a6d9
shubham4132 <shubhamg3122134@gmail.com>
2023-12-27 11:14:27 +0530

    adding key in formSplitBill

--- src/App.js
+++ src/App.js
@@ -73,6 +73,7 @@ export default function App() {
         <FormSplitBill
           onSplitBill={handleSplitBill}
           selectedFriend={selectedFriend}
+          key={selectedFriend.id}
         />
       )}
     </div>
